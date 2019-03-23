286f86b436fc6f586874d2784b9fdf434f63ef45
Robert Flanagan <robertflanagan95@gmail.com>
2019-03-23 13:08:00 -0400

    starting to display meeting info on the page

--- client/src/pages/meeting.js
+++ client/src/pages/meeting.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import API from "./../utils/API.js";
 
 class Meeting extends Component {
+    state = {
+        title: ""
+    }
 
     componentDidMount() {
         const id = this.props.match.params.id;
@@ -9,6 +12,9 @@ class Meeting extends Component {
         API.getMeetingById(id).then(res => {
             // res.data contains all the meeting info we'll want to display in the component
             console.log(res.data);
+            this.setState({
+                title: res.data.title
+            });
         }).catch(err => console.log(err));
     }
 
@@ -17,6 +23,7 @@ class Meeting extends Component {
       return (
         <div>
             <span>looking for one particular meeting...</span>
+            <h1>{this.state.title}</h1>
         </div>
       );
     }
