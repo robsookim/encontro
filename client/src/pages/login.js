b6b25a21800d87041137aca018ecee9d49288ac0
robin kim <robsookim@gmail.com>
2019-03-23 10:18:53 -0400

    create account page w/ css

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import '../App.css';
+
 // import { Input, TextArea, FormBtn } from "./../components/Form";
 // import API from "./../utils/API.js";
 
@@ -24,15 +26,84 @@ class Login extends Component {
     handleFormSubmit(event) {
       event.preventDefault();
     }
-  
+
     render() {
-        return (
-            <div>
-                <a href="http://localhost:3001/auth/linkedin">CLICK HERE for linkedin login</a>
-                <br></br>
-                <a href="http://localhost:3001/auth/google">CLICK HERE for google login</a>
+      return <div className="wrapper">
+        <div className="form-wrapper">
+          <h1>Create Account</h1>
+          <form onSubmit={this.handleSubmit} noValidate>
+  
+            <div className="firstName">
+              <label htmlFor="firstName">First Name: </label>
+              <input 
+                type="text" 
+                className=" " 
+                placeholder="First Name" 
+                name="firstName"
+                noValidate
+                onChange={this.handleChange}
+              /> 
             </div>
-      );
+  
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name: </label>
+              <input 
+                type="text" 
+                className=" " 
+                placeholder="Last Name" 
+                name="lastName"
+                noValidate
+                onChange={this.handleChange}
+              /> 
+            </div>
+  
+            <div className="email">
+              <label htmlFor="email">Email: </label>
+              <input 
+                type="email" 
+                className=" " 
+                placeholder="Email" 
+                name="email"
+                noValidate
+                onChange={this.handleChange}
+              /> 
+            </div>
+  
+            <div className="password">
+              <label htmlFor="password">Password: </label>
+              <input 
+                type="password" 
+                className=" " 
+                placeholder="Password" 
+                name="password"
+                noValidate
+                onChange={this.handleChange}
+              /> 
+            </div>
+  
+            <div className="createAccount">
+              <button type="submit">Create Account</button>
+
+              <small>Already Have an Account? Log In</small>
+            </div>
+
+
+
+
+            <div className="externalLogin">
+            
+              <a href="http://localhost:3001/auth/linkedin">Login through LinkedIn</a>
+              <a href="http://localhost:3001/auth/google">Login with Google</a>
+
+            </div>
+  
+
+
+
+
+          </form>
+        </div>
+      </div>
     }
   }
 
