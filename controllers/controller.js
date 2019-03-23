fe187389da61dcff2cb7b0572dc0e7a8ef72a5ec
pgerspach <pgerspac@alumni.nd.edu>
2019-03-23 10:16:08 -0400

    get meetings

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,17 +1,20 @@
 const axios = require("axios");
-const db = require("../models/sequelize");
+const db = require("../models");
 
 module.exports = {
 
-    // getMeetings: function(req, res) {
-
-    // },
+    getMeetings: async function(req, res) {
+        const userId = req.passport.session.id;
+        const organizationId = await db.sql.User.findOne({where:{id:userId}, attributes:['organization']});
+        const meetings = await db.sql.Meeting.findAll({where:{OrganizationId:organizationId[0]}});
+        res.send(meetings);
+    },
 
     saveMeeting: function(req, res) {
         const meeting = req.body;
         console.log("CONTROLLER meeting info: " + meeting);
 
-        db.Meeting.create(meeting)
+        db.sql.Meeting.create(meeting)
         .then(dbMeeting => res.json(dbMeeting))
         .catch(err => res.status(422).json(err));
     }
