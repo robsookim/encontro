c863df9445b55972adbfd3553e6720951e7d9942
NycolGomes <nycolgomes@gmail.com>
2019-03-27 17:31:59 -0400

    home page/ css a bit messed up

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,19 +5,34 @@
 import React, { Component } from "react";
 // import { Input, TextArea, FormBtn } from "./../components/Form";
 // import API from "./../utils/API.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../home.css';
 
 class Home extends Component {
   
     render() {
         return (
             <div>
-                <a href="/login">CLICK HERE to see login options</a>
+                {/* <a href="/login">CLICK HERE to see login options</a>
                 <br></br>
                 <a href="/form">CLICK HERE to go straight to the meeting form</a>
                 <br></br>
                 <a href="/meetings">CLICK HERE to see all the meetings</a>
                 <br></br>
-                <a href="/users">CLICK HERE to see all the users</a>
+                <a href="/users">CLICK HERE to see all the users</a> */}
+
+                
+                <div className="container-fluid">
+                  <h1>Encontro</h1>
+                  <div className="jumbotron jumbotron-1 ">
+                    <h2>Join Meeting</h2>
+                    <button type="button" className="btn btn-join">→</button>
+                   </div>
+                  <div className="jumbotron jumbotron-2">
+                    <h2>Create Meeting</h2>
+                    <button type="button" className="btn btn-create">+</button>
+                  </div>
+                </div>
 
             </div>
       );
