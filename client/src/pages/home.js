9e0f806c7c69922e215b4fe56b29c3849b20e53e
pgerspach <pgerspac@alumni.nd.edu>
2019-03-29 15:23:02 -0400

    fixed links

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -13,15 +13,15 @@ class Home extends Component {
     return (
       <div>
 
-        <a href="client/src/pages/login.js">click to see login or create account options</a>
+        <a href="/login">click to see login or create account options</a>
         <br></br>
-        <a href="client/src/pages/create.js">click to go straight to create a new meeting</a>
+        <a href="/create">click to go straight to create a new meeting</a>
         <br></br>
-        <a href="client/src/pages/join.js">click to join an existing meeting</a>
+        <a href="/join">click to join an existing meeting</a>
         <br></br>
-        <a href="client/src/pages/users.js">click  to see all the users</a>
+        <a href="/users">click  to see all the users</a>
         <br></br>
-        <a href="client/src/pages/meeting.js">click to see the meeting page once a part of a meeting</a>
+        <a href="/meeting">click to see the meeting page once a part of a meeting</a>
         
         <div className="container-fluid">
           <h1>Encontro</h1>
