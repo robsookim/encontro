23ee9ef9b83be5381bdf4463e359ccebfef8f714
robin kim <robsookim@gmail.com>
2019-03-30 01:27:04 -0400

    join css basic styling

--- client/src/pages/join.js
+++ client/src/pages/join.js
@@ -7,36 +7,41 @@ import '../join.css';
 class Join extends Component {
     render() {
         return (
-            <div>
-                <div className="container">
-                    <div className="jumbotron">
+
+            <div className="container">
+            
+                <h1>JOIN A MEETING</h1> 
+
+                {/* <div className="jumbotron"> */}
+                    {/* <div className="row">
                         <h1>Join a Meeting</h1>
-                        <div className="meetingName">
-                            <h2>Join by ID</h2>
-                                <form>
-                                    <div className="form-group">
-                                        <label for="meetingNameInput">Meeting Name</label>
-                                        <input type="name" className="form-control" id="meetingNameInput" placeholder="enter meeting name"/>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary">Join</button>
-                                </form>
-                        </div>
+                    </div> */}
 
-                        <hr></hr>
+                <h2>Join by ID</h2>
 
-                        <h2>Join Active Meeting</h2>
-                        <div className="meetingActive">
-                            <ul>
-                                <li>dummy meeting 1</li>
-                                <li>dummy meeting 2</li>
-                                <li>Pat smells meeting</li>
-                            </ul>
-                        </div>
+                <div className="row">
+                    <div className="meetingName">
+                        {/* <h2>Join by ID</h2> */}
+                            <form>
+                                <div className="form-group">
+                                    <label for="meetingNameInput">Meeting Name</label>
+                                    <input type="name" className="form-control" id="meetingNameInput" placeholder="meeting name"/>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Join</button>
+                            </form>
+                    </div>
+                </div>
+
+                <h2>Join Active Meeting</h2>
+
+                <div className="row">
+                    <div className="meetingActive">
+                        <ul>
+                            {/* <li>meeting</li> */}
+                        </ul>
                     </div>
                 </div>
             </div>
-
-            
       );
     }
   }
