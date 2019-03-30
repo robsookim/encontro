9e7b3d6b03934f084c5990b782d8368e338af140
Robert Flanagan <robertflanagan95@gmail.com>
2019-03-29 23:39:17 -0400

    fixing meeting page routing

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,12 @@ const router = require("express").Router();
 module.exports = function(router, db) {
   const controller = require("../controllers/controller")(db);
   router.route("/api/meetings")
-    // this `get` route needs a callback function somewhere along the line? not sure where but the server won't start without it
     .get(controller.getMeetings)
 
     .post(controller.saveMeeting);
 
   router.route("/api/users")
     .get(controller.getUsers)
-
-  router.route("/meeting/:id")
-    .get(controller.getMeetingByID)
 
   return router;
 };
