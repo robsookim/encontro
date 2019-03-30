49f83b03631d92a857da18f2d5f7a5a31e1bf300
pgerspach <pgerspac@alumni.nd.edu>
2019-03-30 01:04:09 -0400

    strugglezzz to get agenda front end thingy working but im on my wayyyy

---
+++ client/src/components/AgendaItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./AgendaItem.css"
+class AgendaItem extends Component {
+
+  render() {
+    return (
+      <div className="agenda-item-wrapper" value = {this.props.level}>
+        <label className = "item-header">
+            <input value = {this.props.inputValue}className = "item-header" placeholder="Item #_" onChange={this.props.onItemChange.bind(this,this.props.me, this.props.parent)}>
+            </input>
+            <button /*onClick={this.props.addItem}*/ className="add-item" onClick={this.props.onAddItem.bind(this, this.props.me,this.props.parent)}>
+                Add item {this.props.parent.join("/") + "|" + this.props.me}
+            </button>
+        </label>
+      </div>
+    );
+  }
+}
+
+export default AgendaItem;
