bb0e99930cced3fd55fd8c872b11ed2b1c70cd8f
pgerspach <pgerspac@alumni.nd.edu>
2019-03-30 01:14:12 -0400

    indenting kinda works

--- client/src/components/AgendaItem.js
+++ client/src/components/AgendaItem.js
@@ -5,7 +5,7 @@ class AgendaItem extends Component {
   render() {
     return (
       <div className="agenda-item-wrapper" value = {this.props.level}>
-        <label className = "item-header">
+        <label className = "item-header" style={{display:"flex", marginLeft:`${this.props.level*10}px`}}>
             <input value = {this.props.inputValue}className = "item-header" placeholder="Item #_" onChange={this.props.onItemChange.bind(this,this.props.me, this.props.parent)}>
             </input>
             <button /*onClick={this.props.addItem}*/ className="add-item" onClick={this.props.onAddItem.bind(this, this.props.me,this.props.parent)}>
