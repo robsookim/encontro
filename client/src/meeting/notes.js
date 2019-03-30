bc8b5bfc0eef270d028e709cf799b63e1eb1fca6
robin kim <robsookim@gmail.com>
2019-03-29 20:08:03 -0400

    basic agenda added

--- client/src/meeting/notes.js
+++ client/src/meeting/notes.js
@@ -3,11 +3,11 @@ import '../meeting.css';
 
 const Notes = ({currentNote, handleChange}) => (
   <section className="notes">
-    {/* <input className="notes__input notes__input_title"
+    <input className="notes__input notes__input_title"
            type="text"
            name="title"
-           onChange={handleChange}
-           value={currentNote.title} /> */}
+          //  onChange={handleChange}
+           value={currentNote.title} />
     <textarea className="notes__input notes__input_text"
               name="text"
               value={currentNote.text}
